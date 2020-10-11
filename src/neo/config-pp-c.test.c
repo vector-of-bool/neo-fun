ec560d1b25d9c5c5a91c58eb4a411d630af338bc
vector-of-bool <vectorofbool@gmail.com>
2020-10-10 20:22:02 -0600

    Fix config-pp tests for C

---
+++ src/neo/config-pp-c.test.c
@@ -0,0 +1,35 @@
+#include <neo/config-pp.hpp>
+
+#define Foo Enabled
+#if !(NEO_IsEnabled(Foo))
+#error "Fail"
+#endif
+
+#if !(!NEO_IsDisabled(Foo))
+#error "Fail"
+#endif
+
+#define foo_SettingVal_Color_Green +6
+#define foo_SettingVal_Color_Red +3
+
+#define foo_Setting_Color Red
+
+#if !(NEO_GetSettingValue(foo, Color) == foo_SettingVal_Color_Red)
+#error "Fail"
+#endif
+
+#if !(NEO_SettingValueEqual(foo, Color, Red))
+#error "Fail"
+#endif
+
+#if !(!NEO_SettingValueEqual(foo, Color, Green))
+#error "Fail"
+#endif
+
+#if NEO_SettingValueEqual(foo, Color, Green)
+#error "Fail"
+#endif
+
+#if !NEO_SettingValueEqual(foo, Color, Red)
+#error "Fail2"
+#endif
