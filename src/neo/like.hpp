552e8368d907d0edef3a8c87186fe769bb6c20c5
vector-of-bool <vectorofbool@gmail.com>
2024-03-27 20:03:37 -0600

    Add forward_like_tuple_t, and fix forward_like for void

--- src/neo/like.hpp
+++ src/neo/like.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "./attrib.hpp"
 #include "./type_traits.hpp"
 
 namespace neo {
@@ -12,61 +13,61 @@ struct forward_like_impl;
 template <>
 struct forward_like_impl<true, 1> {
     template <typename T>
-    using merge = remove_reference_t<T> const&;
+    using merge = add_lvalue_reference_t<add_const_t<remove_reference_t<T>>>;
 
     template <typename T>
-    using tuple = T const&;
+    using tuple = add_lvalue_reference_t<add_const_t<T>>;
 
     template <typename T>
-    using language = T const&;
+    using language = add_lvalue_reference_t<add_const_t<T>>;
 };
 
 template <>
 struct forward_like_impl<true, 0> {
     template <typename T>
-    using merge = remove_reference_t<T> const&&;
+    using merge = add_rvalue_reference_t<add_const_t<remove_reference_t<T>>>;
 
     template <typename T>
-    using tuple = T const&&;
+    using tuple = add_rvalue_reference_t<add_const_t<T>>;
 
     template <typename T>
-    using language = T const&;
+    using language = add_lvalue_reference_t<add_const_t<T>>;
 };
 
 template <>
 struct forward_like_impl<false, 1> {
     template <typename T>
-    using merge = remove_reference_t<T>&;
+    using merge = add_lvalue_reference_t<remove_reference_t<T>>;
 
     template <typename T>
-    using tuple = T&;
+    using tuple = add_lvalue_reference_t<T>;
 
     template <typename T>
-    using language = T&;
+    using language = add_lvalue_reference_t<T>;
 };
 
 template <>
 struct forward_like_impl<false, 0> {
     template <typename T>
-    using merge = remove_reference_t<T>&&;
+    using merge = add_rvalue_reference_t<remove_reference_t<T>>;
 
     template <typename T>
-    using tuple = T&&;
+    using tuple = add_rvalue_reference_t<T>;
 
     template <typename T>
-    using language = T&;
+    using language = add_lvalue_reference_t<T>;
 };
 
 template <>
 struct forward_like_impl<false, 2> : forward_like_impl<false, 0> {
     template <typename T>
-    using language = T&;
+    using language = add_lvalue_reference_t<T>;
 };
 
 template <>
 struct forward_like_impl<true, 2> : forward_like_impl<true, 0> {
     template <typename T>
-    using language = const T&;
+    using language = add_lvalue_reference_t<add_const_t<T>>;
 };
 
 template <typename T>
@@ -83,8 +84,23 @@ using model = forward_like_impl<const_type<remove_reference_t<T>>,
 template <typename T, typename U>
 using forward_like_t = like_detail::model<T>::template merge<U>;
 
+/**
+ * @brief Use a forward-like as if we were calling get<>() with a tuple.
+ *
+ * @tparam Owner The "owner" of the object.
+ * @tparam T the type to be transformed.
+ *
+ * - If `T` is a non-reference type, the cvref qualifiers from `Owner` will be applied
+ *   to `T`.
+ * - Otherwise, the cv-qualifiers from `Owner` are ignored, and the reference qualifiers
+ *   of `Owner` are concatenated with the reference qualifiers of `T` and the resulting
+ *   collapsed reference type is the final result.
+ */
+template <typename Owner, typename T>
+using forward_like_tuple_t = like_detail::model<Owner>::template tuple<T>;
+
 template <typename T, typename U>
-constexpr forward_like_t<T, U> forward_like(U&& arg) noexcept {
+NEO_ALWAYS_INLINE constexpr forward_like_t<T, U> forward_like(U&& arg) noexcept {
     return static_cast<forward_like_t<T, U>>(arg);
 }
 
