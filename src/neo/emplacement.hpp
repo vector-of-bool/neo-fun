dfdde620b7d6e447754688015d71555e770e4eb5
vector-of-bool <vectorofbool@gmail.com>
2024-03-27 19:52:11 -0600

    Deprecate our construct_at/destroy_at

--- src/neo/emplacement.hpp
+++ src/neo/emplacement.hpp
@@ -3,29 +3,24 @@
 #include "./fwd.hpp"
 
 #include <memory>
-#include <new>
 
 namespace neo {
 
 template <typename T, typename... Args>
     requires requires(void* p, Args&&... args) { ::new (p) T(NEO_FWD(args)...); }
-constexpr auto construct_at(T* addr,
-                            Args&&... args)  //
+[[deprecated("Use std::construct_at")]] constexpr auto construct_at(T* addr,
+                                                                    Args&&... args)  //
     noexcept(noexcept(::new((void*)0) T(NEO_FWD(args)...))) {
     return std::construct_at(addr, NEO_FWD(args)...);
 }
 
 template <typename T>
-constexpr void destroy_at(T* addr) {
-    addr->~T();
+[[deprecated("Use std::destroy_at")]] constexpr void destroy_at(T* addr) {
+    std::destroy_at(addr);
 }
 
-#if defined __GNUC__ || defined _MSC_VER
-// GCC, Clang, and MSVC all support constexpr placement-new, so we'll just expand a macro for it
-#define NEO_CONSTRUCT_AT(Address, ...) (::std::construct_at(Address __VA_OPT__(, ) __VA_ARGS__))
-#else
 #define NEO_CONSTRUCT_AT ::neo::construct_at
-#endif
+
 // No compiler lets this "just work"
 #define NEO_DESTROY_AT (::neo::destroy_at)
 
