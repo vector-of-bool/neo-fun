131b8e4a303dcb72a55e305857f5979cf114e81a
vector-of-bool <vectorofbool@gmail.com>
2024-03-28 19:56:39 -0600

    Add constexpr_union.hpp

---
+++ src/neo/constexpr_union.test.cpp
@@ -0,0 +1,62 @@
+#include <neo/constexpr_union.hpp>
+
+#include <catch2/catch.hpp>
+
+#include <memory>
+
+template <std::size_t N, typename... Ts, typename... Args>
+constexpr auto make_union(Args&&... args) {
+    using onion_type = neo::constexpr_union<Ts...>;
+    onion_type onion;
+    std::construct_at(&onion.template get<N>(), args...);
+    onion.template construct<N>(args...);
+    using T  = typename onion_type::template nth_type<N>;
+    auto dup = onion.template get<N>();
+    onion.template get<N>().~T();
+    return dup;
+}
+
+TEST_CASE("Create a simple union") {
+    neo::constexpr_union<int> a;
+    a.get<0>() = 31;
+    CHECK(a.get<0>() == 31);
+
+    auto q = make_union<1, double, int, int>(31);
+    CHECK(q == 31);
+
+    constexpr auto Q = make_union<1, double, int, int>(31);
+    STATIC_REQUIRE(Q == 31);
+}
+
+TEST_CASE("Large") {
+    // clang-format off
+    constexpr auto x = make_union<31, int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int,
+                                      int, int, int, int, int, int, int, int, int>(42);
+    // clang-format on
+    STATIC_REQUIRE(x == 42);
+}
+
+struct nontrivial {
+    constexpr nontrivial() {}
+    constexpr ~nontrivial() {}
+    int value = 31;
+};
+
+TEST_CASE("Non-trivial") {
+    neo::constexpr_union<std::string, int> a;
+    a.get<1>() = 31;
+    CHECK(a.get<1>() == 31);
+    std::construct_at(&a.get<0>(), "hey");
+    CHECK(a.get<0>() == "hey");
+
+    {
+        constexpr auto n = make_union<1, int, nontrivial, std::string>();
+        STATIC_REQUIRE(n.value == 31);
+    }
+}
